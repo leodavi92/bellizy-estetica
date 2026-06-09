abf6912bfb0b12d22d175031efb01d1788e21664
Leandro Campos <lleandrodcampos@gmail.com>
2026-06-09 17:44:51 -0400

    fix: ajustar configuração de ícones para respeitar transparência do PNG

--- vite.config.js
+++ vite.config.js
@@ -24,18 +24,20 @@ export default defineConfig({
           {
             src: 'icon.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon.png',
-            sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: 'icon.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
